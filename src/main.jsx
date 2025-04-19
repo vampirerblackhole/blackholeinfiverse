94825d0f844e7533c5d5449377198797b1b61fc2
vampirerblackhole <vampirerblackhole@gmail.com>
2025-04-19 14:10:38 +0530

    add new scroll bar stylings

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Removed SimpleBar integration to restore original scroll behavior
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
